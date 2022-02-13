2ca364eb3c2cbd5cba950510640c659f42ca24d1
Kazumasa Utashiro <kaz@utashiro.com>
2022-02-14 00:58:25 +0900

    minil new Getopt::EX::debug

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+Getopt::EX::debug - It's new $module
+
+# SYNOPSIS
+
+    use Getopt::EX::debug;
+
+# DESCRIPTION
+
+Getopt::EX::debug is ...
+
+# LICENSE
+
+Copyright (C) Kazumasa Utashiro.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+Kazumasa Utashiro <kaz@utashiro.com>
---
+++ lib/Getopt/EX/debug.pm
@@ -0,0 +1,39 @@
+package Getopt::EX::debug;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Getopt::EX::debug - It's new $module
+
+=head1 SYNOPSIS
+
+    use Getopt::EX::debug;
+
+=head1 DESCRIPTION
+
+Getopt::EX::debug is ...
+
+=head1 LICENSE
+
+Copyright (C) Kazumasa Utashiro.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+Kazumasa Utashiro E<lt>kaz@utashiro.comE<gt>
+
+=cut
+
